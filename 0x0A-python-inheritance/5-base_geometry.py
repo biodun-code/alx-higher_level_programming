ac31b15d8eb40e1d0e5e8269936fddbe9af5dffb
majorshuga <biodunbiodun0000@gmail.com>
2022-10-26 20:40:11 +0100

    l

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-This module implements an object skeleton for BaseGeometry
-"""
+
+"""Defines an empty class BaseGeometry."""
 
 
 class BaseGeometry:
-    """skeleton
-    """
+    """Represent base geometry."""
     pass
